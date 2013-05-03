45018e4c1123c1dfc9b302ea3b09ee3d0cf5c072
Nick Doiron <ndoiron@mapmeld.com>
2013-05-03 01:11:44 -0500

    fix some database confusion

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.get('/', function(req, res){
 });
 
 app.get('/createtables', function(req, res){
-  //client.query('CREATE TABLE customgeos (geom geometry, updated timestamp)', function(err, result){
-  //  res.write(JSON.stringify(result || err.toString()));
+  client.query('CREATE TABLE customgeos (geom geometry, updated timestamp)', function(err, result){
+    res.write(JSON.stringify(result || err.toString()));
     client.query('CREATE TABLE timepoints (point geometry, starttime timestamp, endtime timestamp)', function(err, result){
       res.write(JSON.stringify(result || err.toString()));
       res.end();
     });
-  //});
+  });
 });
 
 app.post('/customgeo', function(req, res){
@@ -65,8 +65,8 @@ app.post('/customgeo', function(req, res){
   var now = (new Date()).toISOString().replace("T", " ");
   now = now.substring(0, now.indexOf("Z"));
 
-  client.query("INSERT INTO timepoints VALUES ('" + wkt + "', '" + now + "')", function(err, result){
-    res.send(err || result);
+  client.query("INSERT INTO customgeos VALUES ('" + wkt + "', '" + now + "')", function(err, result){
+    res.send({ "_id": result.oid });
   });
 });
 
