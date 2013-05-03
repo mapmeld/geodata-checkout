1d590432029277eaa088f58060e3792108cde9c3
Nick Doiron <ndoiron@mapmeld.com>
2013-05-02 23:52:20 -0500

    adapting for postgis

--- config.js
+++
@@ -1,6 +0,0 @@
-//------------------------------------
-// Default (development) config values
-//------------------------------------
-
-// MongoDB connection URI for the app
-exports.default_db_uri = "mongodb://localhost/poang";
